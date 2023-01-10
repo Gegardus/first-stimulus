5fd154e650fc6f363ae9c00828e005e8b5ea18e5
Gegardus <stgegard@gmail.com>
2023-01-10 14:57:09 +0400

    first commit

---
+++ app/javascript/controllers/counter_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="counter"
+export default class extends Controller {
+  static targets = ["output"];
+  connect() {   
+    this.count = 0;
+  }
+
+  increment() {    
+    this.count += 1;
+    this.outputTarget.textContent = `You have clicked only ${this.count} time(s)! Don't stop it!`
+  }
+}
+
+
